3ab488ec46eb095502f6335f86f4a33cae45b64e
Christophe <christophe@pop-os.localdomain>
2020-05-29 07:34:18 +0200

    Add  test with only one proxy

---
+++ test/test-proxy.js
@@ -0,0 +1,24 @@
+const { expect } = require('chai');
+const serp = require('../index.js');
+
+describe.skip('Test Simple Search', async () => {
+  it('expect return 10 links with a minimal option set', async () => {
+    const options = {
+      qs: {
+        q: 'test'
+      },
+      proxy: 'http://user:password@host'
+    };
+
+    try {
+      const links = await serp.search(options);
+
+      console.log(links);
+
+      expect(links).to.have.lengthOf(10);
+    } catch (e) {
+      console.log('Error', e);
+      expect(e).be.null;
+    }
+  });
+});
